fdbd8c7da6b7974f779c3ca57d7b46626b884e47
Prakshal Jain <prakshalj0512@gmail.com>
2020-04-03 00:03:25 -0700

    [UPDATE] refactored

--- examples/dags/dag_with_glue_operators.py
+++ examples/dags/dag_with_glue_operators.py
@@ -12,46 +12,35 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
-dag = DAG(
-    'dag_with_glue_operators',
-    default_args=default_args,
-    description='Sample DAG with AWS Glue operators',
-    schedule_interval=None,
-)
 
-start = DummyOperator(
-    task_id='start',
-    dag=dag
-)
+with DAG('dag_with_glue_operators', default_args=default_args, description='Sample DAG with AWS Glue operators', schedule_interval=None) as dag:
 
-glue_job_operator = StartGlueJobRunOperator(
-    task_id='glue_job_operator',
-    job_name='airflow',
-    polling_interval=10,
-    dag=dag
-)
+    start = DummyOperator(
+        task_id='start',
+    )
 
-glue_workflow_operator = StartGlueWorkflowRunOperator(
-    task_id='glue_workflow_operator',
-    workflow_name='airflow',
-    polling_interval=10,
-    dag=dag
-)
+    glue_job_operator = StartGlueJobRunOperator(
+        task_id='glue_job_operator',
+        job_name='airflow',
+        polling_interval=10,
+    )
 
-glue_crawler_operator = StartGlueCrawlerOperator(
-    task_id='glue_crawler_operator',
-    crawler_name='airflow',
-    polling_interval=10,
-    dag=dag
-)
+    glue_workflow_operator = StartGlueWorkflowRunOperator(
+        task_id='glue_workflow_operator',
+        workflow_name='airflow',
+        polling_interval=10,
+    )
 
-glue_trigger_operator = StartGlueTriggerOperator(
-    task_id='glue_trigger_operator',
-    trigger_name='airflow',
-    dag=dag
-)
+    glue_crawler_operator = StartGlueCrawlerOperator(
+        task_id='glue_crawler_operator',
+        crawler_name='airflow',
+        polling_interval=10,
+    )
 
-start >> glue_job_operator
-start >> glue_workflow_operator
-start >> glue_crawler_operator
-start >> glue_trigger_operator
+    glue_trigger_operator = StartGlueTriggerOperator(
+        task_id='glue_trigger_operator',
+        trigger_name='airflow',
+    )
+
+    start >> [glue_job_operator, glue_workflow_operator,
+              glue_crawler_operator, glue_trigger_operator]
